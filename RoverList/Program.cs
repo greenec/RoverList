753dee47b941630748d9ce424880e68ced5f20c1
Connor Greene <connor.greene2000@gmail.com>
2017-11-28 11:52:18 -0500

    initial commit - partially working class

--- RoverList/Program.cs
+++ RoverList/Program.cs
@@ -13,7 +13,28 @@ namespace RoverList
             // TODO:  Implement the RoverList class
             // TODO:  Create a RoverList and then fill it with 16 words
 
+            RoverList myList = new RoverList();
+            myList.Add("apple");
+            myList.Add("bottom");
+            myList.Add("jeans");
+            myList.Add("boots");
+            myList.Add("with");
+            myList.Add("the");
+            myList.Add("fur");
+            myList.Add("she");
+            myList.Add("hit");
+            myList.Add("floor");
+            myList.Add("next");
+            myList.Add("thing");
+            myList.Add("you");
+            myList.Add("know");
+            myList.Add("shawty");
+            myList.Add("low");
+
             // TODO:  Print out the list
+
+            myList.ListNodes();
+            Console.ReadLine();
 
             // TODO:  Remove every 3rd word
             // TODO:  Print out the list
--- RoverList/RoverList.cs
+++ RoverList/RoverList.cs
@@ -9,6 +9,7 @@ namespace RoverList
     class RoverList : RoverListBase
     {
         // Add any variables you need here
+        Node tail;
         
         public RoverList ()
         {
@@ -19,27 +20,54 @@ namespace RoverList
 
         public override void Add(object data)
         {
-            throw new NotImplementedException();
+            if(this.head == null)
+            {
+                this.head = new Node(data);
+                this.tail = this.head;
+                this.tail.Next = null;
+            } else
+            {
+                this.tail.Next = new Node(data);
+                this.tail = this.tail.Next;
+                this.tail.Next = null;
+            }
         }
 
         public override void Add(int Position, object data)
         {
-            throw new NotImplementedException();
+            this.current = this.head;
+            for(int i = 0; i < Position; i++)
+            {
+                this.current = this.current.Next;
+            }
+            this.current.Next = new Node(data);
+            this.tail = this.current.Next;
+            this.tail.Next = null;
         }
 
         public override void Clear()
         {
-            throw new NotImplementedException();
+            this.head = null;
         }
 
         public override Node ElementAt(int Position)
         {
-            throw new NotImplementedException();
+            this.current = this.head;
+            for (int i = 0; i < Position; i++)
+            {
+                this.current = this.current.Next;
+            }
+            return this.current;
         }
 
         public override void ListNodes()
         {
-            throw new NotImplementedException();
+            this.current = this.head;
+            while(this.current.Next != null)
+            {
+                Console.WriteLine(this.current.Data);
+                this.current = this.current.Next;
+            }
         }
 
         public override bool RemoveAt(int Position)
